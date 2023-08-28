d66f08677c161b570f97fa9181ed63b0a11d297a
valensixx <valensixx@gmail.com>
2023-08-28 13:55:30 +0300

    server.js done

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+app.use('/', express.static(path.join(__dirname, '/public'))) 
+//__dirname is global var for nodejs - it says look inside the falder we are in
+
+app.listen(PORT, () => console.log(`Server is runnung on port ${PORT}`));
