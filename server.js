419446c3839211d3d5ed7e4a84e7606759f26b07
valensixx <valensixx@gmail.com>
2023-08-28 14:54:25 +0300

    error 404 logics and html done

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ const PORT = process.env.PORT || 3500;
 app.use('/', express.static(path.join(__dirname, '/public'))) 
 //__dirname is global var for nodejs - it says look inside the falder we are in
 
-app.use('/',require('./routes/root'))
+app.use('/',require('./routes/root'));
+
+app.all('*', (req, res)=>{
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({message:'404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+});
 
 app.listen(PORT, () => console.log(`Server is runnung on port ${PORT}`));
