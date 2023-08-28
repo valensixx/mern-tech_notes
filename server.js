0b70299beae0db331b81f2515da3faabcbd81592
valensixx <valensixx@gmail.com>
2023-08-28 14:17:20 +0300

    add some css and root.js

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ const PORT = process.env.PORT || 3500;
 app.use('/', express.static(path.join(__dirname, '/public'))) 
 //__dirname is global var for nodejs - it says look inside the falder we are in
 
+app.use('/',require('./routes/root'))
+
 app.listen(PORT, () => console.log(`Server is runnung on port ${PORT}`));
