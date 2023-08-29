f3a739386f2d053742fb8e9f18b80bd6cd889d79
valensixx <valensixx@gmail.com>
2023-08-29 13:51:57 +0300

    working on errorHandler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,4 +2,11 @@ const {logEvents} = require('./logger')
 
 const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}\t${req.method}\t{req.url}\t${req.handlers.origin}`, 'errLog.log')
+    console.log(err.stack)
+
+    const status = req.statusCode ? res.statusCode : 500 //server error
+
+    res.status(status) 
+
+    res.json({message: err.message})
 }
