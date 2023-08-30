b68578d34dc3c4790e96da953ec118ec26ad9df4
valensixx <valensixx@gmail.com>
2023-08-30 12:54:37 +0300

    working on fixing server errors

--- routes/root.js
+++ routes/root.js
@@ -4,6 +4,6 @@ const path = require('path')
 
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
-});
+})
 
 module.exports = router
