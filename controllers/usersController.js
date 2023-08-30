5fc1db26b9aef054c417cc9f6ac6d24aaa4dcd22
valensixx <valensixx@gmail.com>
2023-08-30 11:58:32 +0300

    working on userController logics

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,14 +7,42 @@ const bcrypt = require('bcrypt')
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-
+    const users = await User.find().select('-password').lean()
+    if(!users){
+        return res.status(400).json({message: 'No users found'})
+    }
+    res.json(users)
 })
 
 // @desc Create new user
 // @route POST /users
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
+    const {username, password, roles} = req.body
 
+    //Confirm data
+    if(!username || !password || !Array.isArray(roles) || roles.length) {
+        return res.status(400).json({message: 'All fields are required'})
+    }
+
+    //Check for duplicate
+    const duplicate = await User.findOne({username}).lean().exac()
+
+    if(duplicate){
+        return res.status(409).json({message: 'Duplicate username'})
+    }
+
+    //Hash the password 
+    const hashedPwd = await bcrypt.hash(password, 10) //salt rounds
+
+    const userObject = {username, "password": hashedPwd, roles}
+
+    //Create and store new user 
+    const user = await User.create(userObject)
+
+    if(user){ //created
+        res.status(201).json({message: `New user ${username} created`})
+    }
 })
 
 // @desc Update user
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const usersControler = require('../controllers/usersController')
 
 router.route('/')
-    .get() //match as crud --> read
-    .post() // --> create
-    .patch() // --> update 
-    .delete() // --> delete
+    .get(usersController.getAllUsers) //match as crud --> read
+    .post(usersController.createNewUser) // --> create
+    .patch(usersController.updateUser) // --> update 
+    .delete(usersController.deleteUser) // --> delete
 
 module.exports = router
