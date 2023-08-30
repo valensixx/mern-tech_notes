0d9c8cd2285aae5af684fe96c5aa7025cc1f7a66
valensixx <valensixx@gmail.com>
2023-08-30 12:27:35 +0300

    add updated logics

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -42,6 +42,8 @@ const createNewUser = asyncHandler(async (req, res) => {
 
     if(user){ //created
         res.status(201).json({message: `New user ${username} created`})
+    } else {
+        res.status(400).json({message: 'Invalid user data recived'})
     }
 })
 
@@ -49,14 +51,66 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @route PATCH /users
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
+    const {id, username, roles, active, password} = req.body
 
+    //confirm data
+    if(!id || !username || Array.isArray(roles) || !roles.length || typeof active !== 'boolean'){
+        return res.status(400).json({message: 'All fields are required'})
+    }
+
+    const user = await User.findById(id).exac()
+
+    if(!user) {
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    //Check of duplicate 
+    const duplicate = await User.findOne({username}).lean().exac()
+    //Allaw updates to the original user
+    if(duplicate && duplicate?._id.toString() !== id){
+        return res.status(409).json({message: 'Duplicate username'})
+    }
+
+    user.username = username
+    user.roles = roles 
+    user.active = active
+
+    if(password){
+        //hash password
+        user.password =  await bcrypt.hash(password, 10) //salt rounds
+    }
+
+    const updateUser = await user.save()
+
+    res.json({message: `${updateUser.username} update`})
 })
 
 // @desc Delete user
 // @route DELETE /users
 // @access Private 
 const deleteUser = asyncHandler(async (req, res) => {
+    const {id} = req.body
 
+    if(!id){
+        return res.status(400).json({message: 'User ID Required'})
+    }
+
+    const notes = await Note.findOne({user:id}).lean().exac()
+    if(notes?.length){
+        return res.status(400).json({message: 'User has assigned notes'})
+    }
+
+    const user = await User.findById(id).exac()
+
+    if(!user){
+        return res.status(400).json({message: 'User not found'})
+    }
+
+    const result = await user.deleteOne()
+
+    const reply = `Username ${result.username} with ID ${result._id} delete`
+
+    res.json(reply)
 })
 
 module.exports = {
