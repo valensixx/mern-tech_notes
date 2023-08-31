8b0b4246cd4687b4884dff69ced4bdda47296856
valensixx <valensixx@gmail.com>
2023-08-31 13:35:40 +0300

    working on notesCntroller logics

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -6,6 +6,50 @@ const asyncHandler = require('express-async-handler')
 // @route GET /notes
 // @access Private
 
-const getAllNotes = asyncHandler(async (req, res)=>{
+const getAllNotes = asyncHandler(async (req, res) => {
+    //Get all notes form MongoDB
+    const notes = await Note.find().lean()
+
+    //If no notes
+    if (!notes?.length) {
+        return res.status(400).json({ message: 'No notes found' })
+    }
+
+    // Add username to each note before sending the response 
+    // See Promise.all with map() here: https://youtu.be/4lqJBBEpjRE 
+    // You could also do this with a for...of loop
+    const notesWithUser = await Promise.all(notes.map(async (note) => {
+        const user = await User.findById(note.user).lean().exec()
+        return { ...note, username: user.username }
+    }))
+
+    res.json(notesWithUser)
+})
+
+// @desc Create new note
+// @route POST /notes
+// @access Private
+const createNewNote = asyncHandler(async (req, res)=> {
+    const { user, title, text } = req.body
     
+      // Confirm data
+      if (!user || !title || !text) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    // Check for duplicate title
+    const duplicate = await Note.findOne({ title }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: 'Duplicate note title' })
+    }
+
+    // Create and store the new user 
+    const note = await Note.create({ user, title, text })
+
+    if (note) { // Created 
+        return res.status(201).json({ message: 'New note created' })
+    } else {
+        return res.status(400).json({ message: 'Invalid note data received' })
+    }
 })
